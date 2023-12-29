6f0207f2ff19c0737e59fcb2829f0860046eee5a
Ojas Bidkar <155043147+ojasss11@users.noreply.github.com>
2023-12-29 07:30:26 +0530

    practice1Java.java
    Added some code that was missing!

--- practice1Java.java
+++ practice1Java.java
@@ -3,7 +3,41 @@ import java.util.Scanner;
 public class practice1Java {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+System.out.println("Enter any 2 numbers for sum:");
+int a=sc.nextInt();
+int b=sc.nextInt();
+int sum=a+b;
+System.out.println("Sum of 2 numbers is:"+sum);
+System.out.println("TO CALCULATE SIMPLE INTERSEST:");
+System.out.println("Enter the principle amount:");
+float principle=sc.nextFloat();
+System.out.println("Enter the rate:");
+float rate=sc.nextFloat();
+System.out.println("Enter the duration(time):");
+float time=sc.nextFloat();
+float si=(principle*rate*time)/100;
+System.out.println("The simple interst is: "+si);
+System.out.println("\t");
+System.out.println("Enter 3 number's to check who is greater::");
+int num1=sc.nextInt();
+int num2=sc.nextInt();
+int num3=sc.nextInt();
+if(num1>num3 &&num1>num2){
+    System.out.println(num1+" is greatest among the three");
+}
+else if(num2>num3&& num2>num1){
+    System.out.println(num2+" is greatest among the three");
+}
+else{
+    System.out.println(num3+" s greatest among the three");
+}
+System.out.println("Enter the number to fill it's sum till N:");
+int n=sc.nextInt();
+int sum2=0;
+for(int i=0;i<=n;i++){
+    sum2+=i;
+}
+System.out.println("The sum of first "+n+" numbers is:"+sum2);
         // Call the isPrime function to check if a number is prime
         System.out.println("Enter any number");
         int num = sc.nextInt();
